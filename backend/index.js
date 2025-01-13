f8bf3e2d750edb3480fd099f1a28865807a60550
Samir Warsi <warsisamir78.com>
2025-01-13 22:20:53 +0530

    new changes

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true
 }))
-app.use(express.static(path.join(__dirname, "../frontend/dist")))
+app.use(express.static(path.join(__dirname, "dist")))
 app.use(express.json())
 app.use(cookieParser());
 
